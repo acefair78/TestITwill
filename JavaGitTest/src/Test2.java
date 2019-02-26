3970af05cd34e3a27d8f7615b60a12f184563fc8
JOOYONGHEE <JOOYONGHEE@DESKTOP-9TO4O69>
2019-02-26 21:18:44 +0900

    testccc

--- JavaGitTest/src/Test2.java
+++ JavaGitTest/src/Test2.java
@@ -2,8 +2,11 @@
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello!!");
+		System.out.println("Hello!!");
+		System.out.println("Hello!!");
+		System.out.println("Hello!!");
+		System.out.println("Hello!!");
 	}
 
 }
